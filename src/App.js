fb68f1dcfc70d51d20b084ee5d5cb81e33085b45
paulohml <paulo3385@hotmail.com>
2020-09-15 23:34:44 -0400

    melhorias de layout dos posters de filmes divido por categorias

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
-import React from 'react';
-import Row from './Row';
-import './App.css';
+import React from "react";
+import Row from "./Row";
+import "./App.css";
+import requests from "./requests";
 
 function App() {
   return (
     <div className="App">
       <h1>paulohml</h1>
-      <Row title="NETFLIX ORIGINALS"/>
-      <Row title="Trending Now"/>
+      <Row
+        title="Originais NETFLIX"
+        fetchUrl={requests.fetchNetflixOriginals}
+      />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Ação" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Romances" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentários" fetchUrl={requests.fetchDocumentaries} />
+      <Row title="Horror" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Comédia" fetchUrl={requests.fetchComedyMovies} />
     </div>
   );
 }
 
 export default App;
-
--- src/Row.js
+++ src/Row.js
@@ -1,15 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import axios from "./axios";
+import "./Row.css";
 
-function Row({ title }) {
-    return (
-        <div>
-            <h2>{title}</h2>
+const base_url = "http://image.tmdb.org/t/p/original/";
 
-            {/* container -> posters  */}
+function Row({ title, fetchUrl }) {
+  const [filmes, setFilmes] = useState([]);
 
+  // acontece quando carregar as Rows
+  useEffect(() => {
+    // Pegando os objetos
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      // http://api.themoviedb.org/3/discover/tv?api_key=${API_KEY}&with_networks=213
 
-        </div>
-    );
+      console.log(request.data.results);
+      setFilmes(request.data.results);
+      return request;
+    }
+
+    fetchData();
+  }, [fetchUrl]);
+
+  console.table(filmes);
+
+  return (
+    <div className="row">
+      <h2>{title}</h2>
+
+      <div className="row_posters">
+        {/* row__posters */}
+
+        {filmes.map((filme) => (
+          <img
+            key={filme.id}
+            className="row_poster"
+            src={`${base_url}${filme.poster_path}`}
+            alt={filme.name}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Row;
