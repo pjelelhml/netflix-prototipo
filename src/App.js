d1fe2f8f99db2a7f8b732a3cce4ace4c239b545b
paulohml <paulo3385@hotmail.com>
2020-09-15 21:55:15 -0400

    resolvendo problema com npm

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import Row from './Row';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>paulohml</h1>
+      <Row title="NETFLIX ORIGINALS"/>
+      <Row title="Trending Now"/>
     </div>
   );
 }
 
 export default App;
+
